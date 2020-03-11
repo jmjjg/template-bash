7a9e36111eb307c0c15ce9373d8fc40d07824cac
Christian BUFFIN <christian.buffin@adullact-projet.coop>
2020-03-11 02:25:04 +0100

    Changed PostgreSQL aliases.

--- config/bash_aliases/psql.sh
+++ config/bash_aliases/psql.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-__psql_su__()
+__db_pg_su_exec__()
 {
     sudo sudo -u postgres psql --no-align --tuples-only --variable=ON_ERROR_STOP=1 --quiet --command="${1}"
     return "${?}"
 }
 
-alias psql_restart='sudo service postgresql restart'
-alias psql_start='sudo service postgresql start'
-alias psql_status='sudo service postgresql status'
-alias psql_stop='sudo service postgresql stop'
-alias psql_su=__psql_su__
+alias db_pg_restart='sudo service postgresql restart'
+alias db_pg_start='sudo service postgresql start'
+#alias db_pg_status='sudo service postgresql status'
+alias db_pg_stop='sudo service postgresql stop'
+alias db_pg_su_exec=__db_pg_su_exec__
